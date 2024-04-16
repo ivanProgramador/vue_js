39e7d967daecb1159023b3a21fc163f218e7d8ad
ivan <ivan-guita@hotmail.com>
2024-04-15 21:57:21 -0300

    informações sobre instalação do vue

---
+++ anotações/Teoria.js
@@ -0,0 +1,14 @@
+/*
+  Vue js é um framewok reativo como React ou Angular, ele usa o dom para manipular elementos 
+  a ideia dele e faciltar a construção de spa´s, Single Page Aplications que não precisam recarregar 
+  a pagina toda para atualizar as informações que estão sendo exibidas.
+
+  O vua js para funcionar precisa
+  - node js para inetrpretar o codigo dele em tempo de execução 
+  - extensão vtur para vsCode não é obrigatorio mas auamenta a produtividade ao usar o framework
+  - Vue cli é um utilizatrio que facilita a criação de um projeto vue por linha de comando
+
+
+
+
+*/
