478e655343f1958aff43073f4721ed96d122235a
ivan <ivan-guita@hotmail.com>
2024-04-16 09:23:47 -0300

    projeto Vue iniciado fim da aula 253

--- anotações/Teoria.js
+++ anotações/Teoria.js
@@ -3,10 +3,13 @@
   a ideia dele e faciltar a construção de spa´s, Single Page Aplications que não precisam recarregar 
   a pagina toda para atualizar as informações que estão sendo exibidas.
 
-  O vua js para funcionar precisa
-  - node js para inetrpretar o codigo dele em tempo de execução 
-  - extensão vtur para vsCode não é obrigatorio mas auamenta a produtividade ao usar o framework
-  - Vue cli é um utilizatrio que facilita a criação de um projeto vue por linha de comando
+  O vue js para funcionar precisa
+  - node js para interpretar o código dele em tempo de execução 
+  - extensão vtur para vsCode não é obrigatório mas aumenta a produtividade ao usar o framework
+  - Vue cli é um utilitario que facilita a criação de um projeto vue por linha de comando
+
+  Comando para iniciar um porjeto vue js 
+  - vue create <nome do projeto>
 
 
 
