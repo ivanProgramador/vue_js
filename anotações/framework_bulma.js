7e3943da9a2c82e556dc33b354596073fcbd1898
ivan <ivan-guita@hotmail.com>
2024-04-21 23:28:35 -0300

    Bulma instalado fim da aula 271

---
+++ anotações/framework_bulma.js
@@ -0,0 +1,30 @@
+/*
+O framework bulma e mais aconselhavel apara usar com o vue porque ele não depende do Jquery,
+como o bootstrap, em abora seja um bom framework autilização do jquery pode dar conflitos com os 
+recursos do vue porque eles usam os mesmo recursos.
+
+por isso o Bulma vai ser usado nesse projeto. 
+
+no bootstrap 5 talvez isso seja corrigido
+
+------------------------------------------------------------
+comando de instalar 
+
+npm install bulma  --save
+
+como referenciar a biblioteca no main.js 
+--------------------------------------------------
+import { createApp } from 'vue'
+import App from './App.vue'
+import "../node_modules/bulma/css/bulma.css"
+
+createApp(App).mount('#app')
+-----------------------------------------------
+
+
+
+
+
+
+
+*/
